4d1e6ec3866af5a70f1fba944411386e61c81ee1
kotbegimot <arthur.misyunas@gmail.com>
2023-10-19 14:49:22 +0200

    Added form data binding for Thymeleaf

--- src/main/java/com/example/urlshortener/controller/ViewController.java
+++ src/main/java/com/example/urlshortener/controller/ViewController.java
@@ -1,22 +1,29 @@
 package com.example.urlshortener.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import com.example.urlshortener.model.URLOriginalModel;
+import com.example.urlshortener.model.URLShortModel;
+import com.example.urlshortener.service.UrlShortenerService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
+@RequiredArgsConstructor
 public class ViewController {
-    @RequestMapping("/showForm")
-    public String showForm() {
+    private final UrlShortenerService shortenerService;
+    @GetMapping("/showForm")
+    public String showForm(Model theModel) {
+        theModel.addAttribute("shortUrlModel", new URLShortModel());
+        theModel.addAttribute("originalUrlModel", new URLOriginalModel());
         return "initial-form";
     }
 
-    @RequestMapping("/processForm")
-    public String processForm(HttpServletRequest request, Model model) {
-        String shortUrl = request.getParameter("shortURL");
-        String originalURL = shortUrl;
-        model.addAttribute("originalURL", originalURL);
+    @GetMapping("/processForm")
+    public String processForm(@ModelAttribute("shortUrlModel") URLShortModel shortUrlModel,
+                              @ModelAttribute("originalUrlModel") URLOriginalModel originalUrlModel) {
+        originalUrlModel.setOriginalURL(shortenerService.getOriginalURL(shortUrlModel.getShortURL()).getOriginalURL());
         return "showOriginalURL";
     }
 }
